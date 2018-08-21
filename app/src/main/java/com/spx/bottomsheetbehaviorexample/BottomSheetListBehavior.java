a90524a5e011df86ff16db185921b01ba16c9e4c
shaopx <shaopx@gmail.com>
2018-08-21 19:32:39 +0800

    commit

---
+++ app/src/main/java/com/spx/bottomsheetbehaviorexample/BottomSheetListBehavior.java
@@ -0,0 +1,115 @@
+package com.spx.bottomsheetbehaviorexample;
+
+
+import android.content.Context;
+
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class BottomSheetListBehavior extends BottomSheetBehavior {
+    private static final String TAG = "BottomSheetListBehavior";
+
+    private boolean anchorTouched = false;
+    private RecyclerView recyclerView;
+
+    public BottomSheetListBehavior() {
+    }
+
+    public BottomSheetListBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setRecyclerView(RecyclerView recyclerView) {
+        this.recyclerView = recyclerView;
+    }
+
+    @Override
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed, int type) {
+        if (dy < 0 && !leaveEventToNestingChild(target)) {
+            Log.d(TAG, "let child have event!");
+            return;
+        }
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
+    }
+
+    private boolean leaveEventToNestingChild(View target) {
+        return (isAnchorTouched() || isChildOnTop((ViewGroup) target));
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(CoordinatorLayout parent, View child, MotionEvent event) {
+        int action = event.getAction();
+        if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_CANCEL) {
+            setAnchorTouched(false);
+        }
+        return super.onInterceptTouchEvent(parent, child, event);
+    }
+
+    @Override
+    public boolean onNestedPreFling(CoordinatorLayout coordinatorLayout, View child, View target, float velocityX, float velocityY) {
+        setAnchorTouched(false);
+        return super.onNestedPreFling(coordinatorLayout, child, target, velocityX, velocityY);
+    }
+
+
+    private boolean isAnchorTouched() {
+        return anchorTouched;
+    }
+
+    public void setAnchorTouched(boolean flag) {
+        anchorTouched = flag;
+    }
+
+
+    private boolean isChildOnTop(ViewGroup target) {
+        if (target == null) {
+            return false;
+        }
+
+        if (recyclerView == null) {
+            View view = target.findViewById(R.id.recyclerView);
+            if (view == null || !(view instanceof RecyclerView)) {
+                throw new RuntimeException("must have a RecyclerView with id[recyclerView] in NestedScrollView!");
+            }
+            recyclerView = (RecyclerView) view;
+        }
+
+
+        RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+        if (layoutManager instanceof LinearLayoutManager) {
+            LinearLayoutManager linearLayoutManager = (LinearLayoutManager) layoutManager;
+            int firstPos = linearLayoutManager.findFirstCompletelyVisibleItemPosition();
+            if (firstPos == 0) {
+                return true;
+            }
+        } else {
+            throw new RuntimeException("must have a RecyclerView with LinearLayoutManager in NestedScrollView!");
+        }
+        return false;
+    }
+
+
+    public void setAnchorView(View anchorView) {
+        anchorView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    setAnchorTouched(true);
+                } else {
+                    setAnchorTouched(false);
+                }
+                return false;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/spx/bottomsheetbehaviorexample/MainActivity.java
@@ -0,0 +1,96 @@
+package com.spx.bottomsheetbehaviorexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomSheetListBehavior behavior;
+
+    List<String> data = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final NestedScrollView bottomSheet = findViewById(R.id.bottom_sheet);
+        ViewGroup.LayoutParams layoutParams = bottomSheet.getLayoutParams();
+        CoordinatorLayout.Behavior b = ((CoordinatorLayout.LayoutParams) layoutParams)
+                .getBehavior();
+        behavior = (BottomSheetListBehavior) b;
+        behavior.setAnchorView(findViewById(R.id.list_title_tv));
+        behavior.setRecyclerView((RecyclerView) findViewById(R.id.recyclerView));
+
+        findViewById(R.id.startBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (behavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                    behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                } else {
+                    behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+
+            }
+        });
+
+        setupRecyclerView();
+    }
+
+    private void setupRecyclerView() {
+
+        for (int i = 0; i < 100; i++) {
+            data.add("asdfasfdasdfasdf" + i);
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new NormalAdapter());
+    }
+
+
+    static class VH extends RecyclerView.ViewHolder {
+        public final TextView title;
+
+        public VH(View v) {
+            super(v);
+            title = v.findViewById(R.id.title_tv);
+        }
+    }
+
+    class NormalAdapter extends RecyclerView.Adapter<VH> {
+
+        public NormalAdapter() {
+        }
+
+        @Override
+        public void onBindViewHolder(VH holder, int position) {
+            holder.title.setText(data.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        @Override
+        public VH onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+            return new VH(v);
+        }
+    }
+}
